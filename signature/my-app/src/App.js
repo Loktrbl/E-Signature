466c68638390dd78ba57924df261d2a77156df08
Loktrbl <conca8048@gmail.com>
2022-10-06 21:31:59 +0700

    fix getURL totrim

--- signature/my-app/src/App.js
+++ signature/my-app/src/App.js
@@ -1,13 +1,11 @@
-import React, { Component, useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import SignatureCanvas from 'react-signature-canvas';
 import './index.css';
 function App() {
   const sigRef = useRef();
   const [signature, setSignature] = useState(null);
 
-  const handleSignatureEnd = () => {
-    setSignature(sigRef.toDataURL());
-  }
+  const trimSignature = () => setSignature(sigRef.current.getTrimmedCanvas().toDataURL("image/png"));
 
   const clearSignature = () => {
     sigRef.current.clear();
@@ -21,13 +19,27 @@ function App() {
   return (<div>
     <SignatureCanvas
       penColor="black"
-      canvasProps={{ className: 'signature' }}
+      canvasProps={{ className: "signature" }}
       ref={sigRef}
     />
+
     <button onClick={clearSignature}>Clear</button>
-    <button onClick={handleSignatureEnd}>Get URL</button>
-    <div style={{ width: "500px", wordBreak: "break-all" }}>{signature}</div>
-  </div>
+    <button onClick={trimSignature}>Trim</button>
+
+    {
+      signature ? (<img
+        src={signature}
+        alt="mi firma"
+        style={{
+          display: "block",
+          height: "200px",
+          margin: "100px",
+          border: "2px solid black",
+          width: "500px",
+        }}
+      />) : null
+    }
+  </div >
   );
 }
 
